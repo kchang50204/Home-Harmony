cc1ad6476943376078161985e7aa51f78f366bb7
Kevin Chang <Kchang50204@csu.fullerton.edu>
2026-04-25 13:44:57 -0700

    add choresview and choreviewmodel with full functionality

--- HomeHarmony/HomeHarmony/HomeHarmonyApp.swift
+++ HomeHarmony/HomeHarmony/HomeHarmonyApp.swift
@@ -3,7 +3,7 @@ import SwiftUI
 @main
 struct HomeHarmonyApp: App {
     var body: some Scene {
-        WindowsGroup {
+        WindowGroup {
             ContentView()
         }
     }
--- HomeHarmony/HomeHarmony/ViewModels/ChoreViewModel.swift
+++ HomeHarmony/HomeHarmony/ViewModels/ChoreViewModel.swift
@@ -1,6 +1,22 @@
 import Foundation
 import Combine
+import SwiftUI
 
 class ChoreViewModel: ObservableObject {
     @Published var chores: [Chore] = []
+    
+    func addChore(title: String, assignedTo: String, dueDate: Date) {
+        let newChore = Chore(title: title, assignedTo: assignedTo, dueDate: dueDate)
+        chores.append(newChore)
+    }
+    
+    func toggleChore(chore: Chore) {
+        if let index = chores.firstIndex(where: { $0.id == chore.id }) {
+            chores[index].isCompleted.toggle()
+        }
+    }
+    
+    func deleteChore(at offsets: IndexSet) {
+        chores.remove(atOffsets: offsets)
+    }
 }
